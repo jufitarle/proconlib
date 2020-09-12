ce0d8fff26073430ef3136b52cc1b9bb6352efc8
jufitarle <70431015+jufitarle@users.noreply.github.com>
2020-09-13 08:47:46 +0900

    Update util.rs

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,3 @@
-
-
 trait VecUtil {
     fn compress(&self) -> Self;
 }
@@ -7,25 +5,18 @@ trait VecUtil {
 impl VecUtil for Vec<char> {
     fn compress(&self) -> Self {
         let n = self.len();
-        let mut res = vec![];
+        let mut res = vec![self[0]];
         let mut count = 1;
         for i in 1..n {
-            if self[i - 1] != self[i] {
-                res.push(self[i - 1]);
-                res = [res, count.to_string().chars().collect()].concat();
-                count = 1;
-                if i == n - 1 {
-                    res.push(self[i]);
-                    res.push('1');
-                }
-            } else {
+            if self[i - 1] == self[i] {
                 count += 1;
-                if i == n - 1 {
-                    res.push(self[i - 1]);
-                    res = [res, count.to_string().chars().collect()].concat();
-                }
+            } else {
+                res.append(&mut count.to_string().chars().collect());
+                res.push(self[i]);
+                count = 1;
             }
         }
+        res.append(&mut count.to_string().chars().collect());
         res
     }
 }
